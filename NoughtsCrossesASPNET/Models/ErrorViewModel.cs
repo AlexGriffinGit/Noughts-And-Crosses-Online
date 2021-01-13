4cc2e2d19b20c2b51812afe9199360ee49d0622b
Alex Griffin <alexgriffin.dev@gmail.com>
2021-01-13 14:02:07 +0000

    ReAdded Initial Commit

---
+++ NoughtsCrossesASPNET/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NoughtsCrossesASPNET.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
